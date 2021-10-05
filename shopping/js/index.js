e5d5a2ab3e445e72b0f7833cac085de6b1b380a4
Huseynveliyev <91878561+Huseynveliyev@users.noreply.github.com>
2021-10-05 22:34:18 +0400

    shopping site

---
+++ shopping/js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:5
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:5
+        }
+    }
+})
